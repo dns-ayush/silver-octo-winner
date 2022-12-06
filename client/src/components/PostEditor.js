4efa899aae12936a2d8b60a64f0678d791530fe0
dns-ayush <119953059+dns-ayush@users.noreply.github.com>
2022-12-06 20:48:20 +0530

    Add files via upload

---
+++ client/src/components/PostEditor.js
@@ -0,0 +1,136 @@
+import {
+  Button,
+  Card,
+  Link,
+  Stack,
+  TextField,
+  Typography,
+  MenuItem
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { createPost } from "../api/posts";
+import ErrorAlert from "./ErrorAlert";
+import { isLoggedIn } from "../helpers/authHelper";
+import HorizontalStack from "./util/HorizontalStack";
+import UserAvatar from "./UserAvatar";
+
+let lat,long;
+navigator.geolocation.getCurrentPosition((position)=>{
+//  lat=position.coords.latitude;
+// long=position.coords.longitude;
+lat=28.6183531 ;
+long=77.3624517;
+});
+const PostEditor = () => {
+  console.log(lat,long);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    location: {
+      coordinates:[
+        lat,
+        long
+      ]
+    },
+  });
+  const [serverError, setServerError] = useState("");
+  const [errors, setErrors] = useState({});
+  const user = isLoggedIn();
+
+  
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const errors = validate();
+    setErrors(errors);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const data = await createPost(formData, isLoggedIn());
+    setLoading(false);
+    if (data && data.error) {
+      setServerError(data.error);
+    } else {
+      navigate("/posts/" + data._id);
+    }
+  };
+
+  const validate = () => {
+    const errors = {};
+
+    return errors;
+  };
+
+  return (
+    <Card>
+      <Stack spacing={1}>
+        {user && (
+          <HorizontalStack spacing={2}>
+            <UserAvatar width={50} height={50} username={user.username} />
+            <Typography variant="h5">
+              What would you like to post today {user.username}?
+            </Typography>
+          </HorizontalStack>
+        )}
+
+        {/* <Typography>
+          <Link href="https://commonmark.org/help/" target="_blank">
+            Markdown Help
+          </Link>
+        </Typography> */}
+
+        <Box component="form" onSubmit={handleSubmit}>
+        <TextField
+            fullWidth
+            select
+            label="Title"
+            required
+            name="title"
+            margin="normal"
+            onChange={handleChange}
+            error={errors.title !== undefined}
+            helperText={errors.title}
+            >
+              <MenuItem value="Fitness">Fitness</MenuItem>
+              <MenuItem value="Food">Food</MenuItem>
+              <MenuItem value="Sports">Sports</MenuItem>
+              <MenuItem value="Grooming">Grooming</MenuItem>
+              <MenuItem value="Education">Education</MenuItem>
+              <MenuItem value="Others">Others</MenuItem>
+            </TextField>
+          <TextField
+            fullWidth
+            label="Content"
+            multiline
+            rows={10}
+            name="content"
+            margin="normal"
+            onChange={handleChange}
+            error={errors.content !== undefined}
+            helperText={errors.content}
+            required
+          />
+          <ErrorAlert error={serverError} />
+          <Button
+            variant="outlined"
+            type="submit"
+            fullWidth
+            disabled={loading}
+            sx={{
+              mt: 2,
+            }}
+          >
+            {loading ? <>Submitting</> : <>Submit</>}
+          </Button>
+        </Box>
+      </Stack>
+    </Card>
+  );
+};
+
+export default PostEditor;
